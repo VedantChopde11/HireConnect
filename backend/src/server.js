cad64927a640e0025789bf15a4736543a98956e7
VedantChopde11 <vedantchopde11@gmail.com>
2026-01-02 19:42:05 +0530

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import path from "path"
 import  {ENV} from "./lib/env.js"
+import { connectDB } from "./lib/db.js"
 
 const app = express()
 
@@ -20,6 +21,17 @@ if(ENV.NODE_ENV === "production") {
     })
 }
 
-app.listen(3000 , () => {
-    console.log("connected to the backend at port:",ENV.PORT)
-})
+
+const startServer = async () => {
+    try {
+       await connectDB()
+       app.listen(ENV.PORT , () => {
+        console.log("connected to the backend at port:",ENV.PORT)
+       })
+
+    } catch (error) {
+        console.error("Database connection failed" , error)
+    }
+}
+
+startServer()

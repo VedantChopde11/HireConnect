12cd91cec90914eacf82e81ecd2baab6550e4686
VedantChopde11 <vedantchopde11@gmail.com>
2026-01-12 11:42:55 +0530

    updating

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,6 +5,5 @@ const axiosInstance = axios.create({
     withCredentials:true // by adding this field browser will send the cookies to server automatically on every single req
 })
 
-console.log("BACCKEND_URL:  " , import.meta.env.VITE_API_URL)
 
 export default axiosInstance

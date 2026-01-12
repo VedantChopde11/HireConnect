1b5c55705455331cf90928a21b20a19ce1fdb43c
VedantChopde11 <vedantchopde11@gmail.com>
2026-01-12 18:48:10 +0530

    refactoring

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -4,7 +4,7 @@ import { createSession, endSession, getActiveSessions, getMyRecentSessions, getS
 
 const router = express.Router()
 
-router.post("/" ,createSession)
+router.post("/" ,protectRoute, createSession)
 router.get("/active" , protectRoute, getActiveSessions)
 router.get("/my-recent" , protectRoute, getMyRecentSessions)
 router.get("/:id" , protectRoute,getSessionById)
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,20 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}/api`,
-    withCredentials:true // by adding this field browser will send the cookies to server automatically on every single req
+    //withCredentials:true // by adding this field browser will send the cookies to server automatically on every single req
 })
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const auth = getAuth();
+  const token = await auth.getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 
 
 export default axiosInstance

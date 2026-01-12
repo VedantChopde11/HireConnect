5949c47f369bd62daf230fda37a19ba160e7dc05
VedantChopde11 <vedantchopde11@gmail.com>
2026-01-12 13:43:23 +0530

    refactoring

--- backend/src/server.js
+++ backend/src/server.js
@@ -63,11 +63,10 @@ app.use("/api/sessions" , sessionRoutes)
 if(ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*" , (req,res) => {
+    app.get("/{*any}" , (req,res) => {
         res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
     })
 }
-
 
 // ===== START SERVER =====
 
